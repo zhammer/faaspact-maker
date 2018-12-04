578a32fb3049aa8c7ed43f698ff412bbcc85bb23
zhammer <zachary_hammer@alumni.brown.edu>
2018-12-04 09:57:15 -0500

    Actually include requirements in pypi package

--- setup.py
+++ setup.py
@@ -2,15 +2,22 @@
 
 from setuptools import setup
 
+from pipenv.project import Project
+from pipenv.utils import convert_deps_to_pip
+
+pipfile = Project(chdir=False).parsed_pipfile
+requirements = convert_deps_to_pip(pipfile['packages'], r=False)
+
 with open('README.md', 'r') as readme:
     long_description = readme.read()
 
 setup(
     name='faaspact-maker',
-    version='0.0.6',
+    version='0.0.7',
     description='Make and mock pacts in python for faas microservices.',
     url='https://github.com/zhammer/faaspact-maker',
     packages=('faaspact_maker',),
+    install_requires=requirements,
     author='Zach Hammer',
     author_email='zach.the.hammer@gmail.com',
     license='MIT License',
