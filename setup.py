2ccdbc541d5cb07f855139d283c8f9afd919a8b5
zhammer <zachary_hammer@alumni.brown.edu>
2018-12-04 13:03:46 -0500

    Use find_packages

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 """Setup for pytest-hammertime plugin."""
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 from pipenv.project import Project
 from pipenv.utils import convert_deps_to_pip
@@ -13,10 +13,10 @@ with open('README.md', 'r') as readme:
 
 setup(
     name='faaspact-maker',
-    version='0.0.7',
+    version='0.0.9',
     description='Make and mock pacts in python for faas microservices.',
     url='https://github.com/zhammer/faaspact-maker',
-    packages=('faaspact_maker',),
+    packages=find_packages(),
     install_requires=requirements,
     author='Zach Hammer',
     author_email='zach.the.hammer@gmail.com',
