71e9f1812d47c7cfdde450a16bbe3ee0e32780ed
zhammer <zachary_hammer@alumni.brown.edu>
2018-12-09 11:31:04 -0500

    Use annotations for class type self-references

--- faaspact_maker/matchers/definitions.py
+++ faaspact_maker/matchers/definitions.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import re
 from dataclasses import dataclass
 from typing import Union
@@ -13,7 +15,7 @@ class Matcher(Protocol):
     # Note: ideally here I'd want an ABC with an abstract property
     # for getting a matcher's value, but that is throwing some runtime
     # errors that i don't know how to debug.
-    def value(self) -> Union[Primitive, 'Matcher']:
+    def value(self) -> Union[Primitive, Matcher]:  # noqa: F821
         ...
 
 
