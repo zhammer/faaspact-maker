f7d492a34c03595dee2d5ab6c9fdca480036e935
zhammer <zachary_hammer@alumni.brown.edu>
2018-12-11 22:01:51 -0500

    0.0.12

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.md', 'r') as readme:
 
 setup(
     name='faaspact-maker',
-    version='0.0.11',
+    version='0.0.12',
     description='Make and mock pacts in python for faas microservices.',
     url='https://github.com/zhammer/faaspact-maker',
     packages=find_packages(),
